9e05a7177915bfa5e787050df31ae8096c31d111
Ngamesh <ngamesh15@gmail.com>
2026-02-22 22:55:11 +1100

    Initial commit

---
+++ js/booking.js
@@ -0,0 +1,8 @@
+
+function openForm() {
+  document.getElementById("popup1").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("popup1").style.display = "none";
+}
---
+++ js/limited.js
@@ -0,0 +1,10 @@
+//js for limited time
+function countdown() {
+    $('#clock').countdown('2021/1/1').on('update.countdown', function(event) {
+      var $this = $(this).html(event.strftime(''
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%D</span> Day%!d</div>'
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%H</span> Hr</div>'
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%M</span> Min</div>'
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%S</span> Sec</div>'));
+    });
+};
