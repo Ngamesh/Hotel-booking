904690583fe0745bd73f73eee5ae4ccb58956fa8
Ngamesh <ngamesh15@gmail.com>
2026-06-14 21:50:52 +1000

    Fix blog sidebar and mobile nav close

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,23 +1,40 @@
 (function ($) {
   "use strict"; // Start of use strict
 
+  function closeResponsiveMenu(callback) {
+    var $menu = $('.navbar-collapse');
+
+    if ($menu.hasClass('show') || $menu.hasClass('collapsing')) {
+      $menu.one('hidden.bs.collapse', function () {
+        if (callback) callback();
+      });
+      $menu.collapse('hide');
+    } else if (callback) {
+      callback();
+    }
+  }
+
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function (event) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 70) // Increased offset to account for nav height/margins
-        }, 1000, "easeInOutExpo");
-        return false;
+        event.preventDefault();
+        event.stopImmediatePropagation();
+
+        closeResponsiveMenu(function () {
+          $('html, body').stop(true).animate({
+            scrollTop: (target.offset().top - 70) // Increased offset to account for nav height/margins
+          }, 850, "easeInOutExpo");
+        });
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function () {
-    $('.navbar-collapse').collapse('hide');
+    closeResponsiveMenu();
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
@@ -27,6 +44,5 @@
   });
 
 })(jQuery); // End of use strict
-
 
 
